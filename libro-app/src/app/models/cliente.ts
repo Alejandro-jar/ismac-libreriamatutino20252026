cf2e4923d7846574bc0e816e36cb0123b2bffc7c
Alejandro <avjaramillo@tecologicoismac.edu.ec>
2026-01-30 12:14:00 -0500

    cambios realizados

--- libro-app/src/app/models/cliente.ts
+++ libro-app/src/app/models/cliente.ts
@@ -1,9 +1,11 @@
-export class Cliente {
-  idCliente: number | null = null;
-  cedula: string = '';
-  nombre: string = '';
-  apellido: string = '';
-  direccion: string = '';
-  telefono: number | null = null;
-  correo: string = '';
+//comentario
+//no pues no vale
+export interface Cliente {
+  idCliente: number | null;
+  cedula: string;
+  nombre: string;
+  apellido: string;
+  direccion: string;
+  telefono: number | null;
+  correo: string;
 }
